1ec4be42db9d2f715d6e3129533790c580b20617
ucup-11 <rayyashss11@gmail.com>
2022-10-13 11:21:43 +0700

    Minggu Kedua

--- Minggu_Kedua/Tugas/tugasArray1.js
+++ Minggu_Kedua/Tugas/tugasArray1.js
@@ -1,11 +1,20 @@
 let barang = ['pulpen' , 'buku', 'klip'];
 
-
-function indexArray(i) {
+function indexArray(j) {
 for(let index in barang){ 
-    if( i == barang[index]){
+    if( j == barang[index]){
         return index;
         }
     }
 }
-console.log(indexArray("pulpen"))
+
+console.log(indexArray("buku"))
+
+function indexArray1(j){
+    for (let i = 0 ; i < barang.length ; i ++){
+        if ( barang [i] == j  ){
+            return i;
+        }
+    }
+}
+console.log(indexArray1("buku"));
